ed7c23d392f136ac3093d7b5cca7d4b723d95009
Jamaal Smith <jamaalsmith@Jamaals-MBP.lan>
2024-02-12 21:21:37 -0600

    starter guide for react query

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
+import { QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
 import { HomePage } from "./pages/HomePage"
 
 function App() {
   return (
     <>
+    <QueryClientProvider>
     <HomePage />
+    </QueryClientProvider>
     </>
   )
 }
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,9 +1,33 @@
 import React from "react";
 import { NotesDrawer } from "../../shared/Notes/NotesDrawer";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 
 export const HomePage = () => {
+
+    const queryClient = useQueryClient()
+
+    const query = useQuery({ queryKey: ["todos"], queryFin: getTodos})
+
+    const mutation = useMutation({mutationFn: postTodo,
+    onSuccess: ()=> {
+        queryClient.invalidateQueries({queryKey: ['todos']})
+    }})
+
     return (
         <div>
+            <ul>
+                {query.data?.map((todo)=> <li key= {todo.id}>{todo.title}</li>)}
+            </ul>
+              <button
+            onClick={()=>{
+            mutation.mutate({
+                id: Date.now(),
+                title: 'Do Laundry',
+            })
+        }}>
+            Add Todo
+        </button>
+
         <h1>Home Page</h1>
         <NotesDrawer/>
         </div>
