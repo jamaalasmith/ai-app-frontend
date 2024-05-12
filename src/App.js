3ffd00eb3ac671f465c93da1490ca7989990d893
Jamaal Smith <jamaalsmith010@gmail.com>
2024-05-11 19:40:35 -0500

    use confirgprovider

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
-import { QueryClientProvider } from '@tanstack/react-query';
 import './App.css';
 import { HomePage } from "./pages/HomePage"
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App() {
   return (
     <>
-    <QueryClientProvider>
     <HomePage />
-    <ReactQueryDevtools initialIsOpen={true} />
-    </QueryClientProvider>
     </>
   )
 }
--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,35 +1,10 @@
-import React from "react";
-import { NotesDrawer } from "../../shared/Notes/NotesDrawer";
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { Demo } from "../../shared/Demo";
 
 export const HomePage = () => {
-
-    const queryClient = useQueryClient()
-
-    const query = useQuery({ queryKey: ["todos"], queryFin: getTodos})
-
-    const mutation = useMutation({mutationFn: postTodo,
-    onSuccess: ()=> {
-        queryClient.invalidateQueries({queryKey: ['todos']})
-    }})
-// testing
-    return (
-        <div>
-            <ul>
-                {query.data?.map((todo)=> <li key= {todo.id}>{todo.title}</li>)}
-            </ul>
-              <button
-            onClick={()=>{
-            mutation.mutate({
-                id: Date.now(),
-                title: 'Do Laundry',
-            })
-        }}>
-            Add Todo
-        </button>
-
-        <h1>Home Page</h1>
-        <NotesDrawer/>
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <Demo />
+    </div>
+  );
+};
